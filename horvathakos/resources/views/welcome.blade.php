ebe09105585c9396de0a348ddc7f2a1d3a3b41de
Akos032 <horako032@hengersor.hu>
2024-09-23 09:28:10 +0200

    Update welcome.blade.php

--- horvathakos/resources/views/welcome.blade.php
+++ horvathakos/resources/views/welcome.blade.php
@@ -19,7 +19,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" href="bejelentkezes">Bejelentkezes</a>
+          <a class="nav-link" href="bejelentkezes">Bejelentkezés</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="bemutatkozas">Bemutatkozás</a>
